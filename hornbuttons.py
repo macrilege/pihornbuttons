84c5b222fffda5183533583fde416ad454a9c8e4
macrilege <macrilege@yahoo.com>
2023-05-01 18:38:06 -0500

    cleanup code and fix typo

--- hornbuttons.py
+++ hornbuttons.py
@@ -1,3 +1,9 @@
+
+#############################
+# Python Horn Sound Player
+# Author Michael McCullough
+#############################
+
 import os
 import time 
 import sleep
@@ -6,16 +12,25 @@ from signal import pause
 from pygame import mixer
 
 count = 0
-relay_pim = 4
+relay_pin = 4
 relay = gpiozero.OutputDevice(relay_pin, active_high=True, initial_value=False)
+
 button_back = Button(20)
 button_forward = Button(21)
 button_play = Button(16)
-sound_array = ['sound1', 'sound2', 'sound3', 'sound4', 'sound5', 'sound6', 'sound7', 'sound8', 'sound9']
-#TODO change to directory of mp3 sounds and build the array from there?
-array_length = len(sound_array)
 
-print(relay.value)
+sound_array = ['1-21-gigawatts', 'doc-88-miles-per-hour', 
+               'mario-coin-sound', 'mario-1up-wii', 
+               'mario-invincible-sound', 'mario-jump-sound', 
+               'pikachu-pika', 
+               'luke-leiya', 'r2d2', 'yoda_doordonot', 'vader-iam-your-father', 'chewbacca', 
+               'chewbaca-1', 'darthvader_circleiscomplete', 
+               'clint-eastwood', 'knight-all-systems', 'knight-rider-theme'
+               'trek-space-the-final', 'star-trek-theme', 
+               'terminator-cybernetic', 'robocop-thankyou', 
+               'thx', 'fifth-element-meat', 'austin_behave']
+
+array_length = len(sound_array)
 print(len(sound_array))
 
 mixer.init()
@@ -28,9 +43,10 @@ def play_sound():
         stop_sound()
     except Exception:
         print("Sound was unable to stop")
+
     relay.on() #Turn on the speaker PA
     time.sleep(.5)
-    mixer.music.load("/home/smartcar/smarthorn/sounds/{0}.mp3").format(sound_array[count])
+    mixer.music.load("./sounds/{0}.mp3").format(sound_array[count])
     mixer.music.play()
     while mixer.music.get_busy():
         time.sleep(1)
@@ -42,7 +58,6 @@ def selec_next(button):
     except Exception:
         print("Sound was unable to stop")
 
-    print(button.pin)
     if (count < array_length):
         count = count + 1
 
@@ -52,13 +67,8 @@ def select_prev(button):
     except Exception:
         print("Sound was unable to stop")
 
-    print(button.pin)
     if (count > 0):
         count = count - 1
-
-def play_sound_old(button):
-    print(button.pin)
-    currentsound = sound_array[count]
 
 button_forward.when_pressed = selec_next
 button_back.when_pressed = select_prev
