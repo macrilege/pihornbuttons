4e22177ca91c5dcde0fdca383c88f1acef82a920
macrilege <macrilege@yahoo.com>
2023-05-01 17:51:25 -0500

    added relay control to turn on the speaker

--- hornbuttons.py
+++ hornbuttons.py
@@ -5,13 +5,17 @@ from gpiozero import Button
 from signal import pause
 from pygame import mixer
 
+count = 0
+relay_pim = 4
+relay = gpiozero.OutputDevice(relay_pin, active_high=True, initial_value=False)
 button_back = Button(20)
 button_forward = Button(21)
 button_play = Button(2)
-count = 0
 sound_array = ['sound1', 'sound2', 'sound3', 'sound4', 'sound5', 'sound6', 'sound7', 'sound8', 'sound9']
+#TODO change to directory of mp3 sounds and build the array from there?
 array_length = len(sound_array)
 
+print(relay.value)
 print(len(sound_array))
 
 mixer.init()
@@ -24,11 +28,13 @@ def play_sound():
         stop_sound()
     except Exception:
         print("Sound was unable to stop")
-
+    relay.on() #Turn on the speaker PA
+    time.sleep(.5)
     mixer.music.load("/home/smartcar/smarthorn/sounds/{0}.mp3").format(sound_array[count])
     mixer.music.play()
     while mixer.music.get_busy():
         time.sleep(1)
+        relay.off()
    
 def selec_next(button):
     try:
@@ -57,5 +63,3 @@ def play_sound_old(button):
 button_forward.when_pressed = selec_next
 button_back.when_pressed = select_prev
 button_play.when_pressed = play_sound
-
-#TODO setup the relays and delays / actions
